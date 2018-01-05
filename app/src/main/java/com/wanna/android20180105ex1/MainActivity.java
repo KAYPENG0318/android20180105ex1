50db53748109eba347746bf939a14b35f86c40ef
kay <ruby50333@yahoo.com.tw>
2018-01-05 14:27:23 +0800

    Initial commit

---
+++ app/src/main/java/com/wanna/android20180105ex1/MainActivity.java
@@ -0,0 +1,188 @@
+package com.wanna.android20180105ex1;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv,tv2,tv3,tv4;
+    EditText ed;
+    int ch=-1,tmp=-1;
+    boolean[] chks = new boolean[5];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.textView);
+        tv2=findViewById(R.id.textView2);
+        tv3=findViewById(R.id.textView3);
+        tv4=findViewById(R.id.textView4);
+    }
+    public void click1(View v)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("This is title");
+        builder.setMessage("Hello");
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"按下了確定",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"按下了取消",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNeutralButton("Help", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"需要幫助",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.show();
+    }
+    public void click2(View v)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("This is title");
+        ed=new EditText(MainActivity.this);
+        ed.setText(tv.getText().toString());
+        builder.setView(ed);
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                tv.setText(ed.getText().toString());
+                Toast.makeText(MainActivity.this,"按下了確定",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"按下了取消",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
+    }
+    public void click3(View v)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("選單");
+        final String coffee[]={"美式","拿鐵","濃縮"};
+        builder.setItems(coffee, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                tv2.setText(coffee[i]);
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
+    public void click4(View v)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("選單");
+        final String coffee[]={"美式","拿鐵","濃縮"};
+        tmp=ch;
+        builder.setSingleChoiceItems(coffee, ch, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                tmp=i;
+            }
+        });
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                ch=tmp;
+                tv3.setText(coffee[ch]);
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
+    public void click5(View v)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("多選列表");
+        final String coffee[]={"美式","拿鐵","濃縮","抹茶拿鐵","榛果拿鐵"};
+        builder.setMultiChoiceItems(coffee, chks, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+
+            }
+        });
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int position) {
+                StringBuilder sb = new StringBuilder();
+                for (int i=0;i<=4;i++)
+                {
+                    if (chks[i])
+                    {
+                        sb.append(coffee[i] + ",");
+                    }
+                }
+                tv4.setText(sb.toString());
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
+    public void click6(View v)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("This is title");
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        View v1= inflater.inflate(R.layout.layout1,null);
+        final TextView tv4 = v1.findViewById(R.id.textView5);
+        Button btn1= v1.findViewById(R.id.button7);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv4.setText("hello world");
+            }
+        });
+        builder.setView(v1);
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Toast.makeText(MainActivity.this, "按下了確定", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.show();
+    }
+}
